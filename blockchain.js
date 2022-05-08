bc970b29cffb10036eecd5b83b3b6755cbfe8e93
Muhammed Ali ÖZKAYA <m_aliozkaya@hotmail.com>
2022-05-08 19:23:55 +0300

    2.video
    Proof of Work implementation

--- blockchain.js
+++ blockchain.js
@@ -13,11 +13,21 @@ class Block{
     calculateHash(){
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')){
+            this.hash = this.calculateHash();
+            this.nonce++;
+        }
+
+        console.log("Mining bitti Hash:" + this.hash);
+    }
 }
 
 class BlockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
 
     createGenesisBlock(){
@@ -33,9 +43,25 @@ class BlockChain{
     
     addBlock(block){
         block.previousHash = this.getLatestBlock().hash;
-        block.hash = block.calculateHash();
         block.index = this.chain.length;
+        block.mineBlock(this.difficulty);
         this.chain.push(block);
+    }
+
+    isChainValid(){
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
